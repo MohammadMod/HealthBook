329c0a932cb3682470e5198c2d61751ed199dcdf
Mohammad Ahmad <mohammad.kurdy.2012@outlook.com>
2019-06-20 21:16:31 +0300

    donate organ sends code

--- HealthBook/Donate Organs.aspx.cs
+++ HealthBook/Donate Organs.aspx.cs
@@ -7,13 +7,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MessageBird;
 
 namespace HealthBook
 {
     public partial class Donate_Organs : System.Web.UI.Page
     {
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ToString());
-
+        Code_Generator g = new Code_Generator();
+        static string myCode = "";
+        static string phone = "";
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -27,74 +30,103 @@ namespace HealthBook
 
         protected void SubmitButton_Click(object sender, EventArgs e)
         {
-            string phone = String.Format("{0}", Request.Form["phone"]);
-            long PhoneNumber = long.Parse(phone);
-            string n = String.Format("{0}", Request.Form["DateOfBirthTextBox"]);
-
-            string selectOrganList = Request.Form["Donatestate"];
-            string selectedOrgan = "";
-
-            if (selectOrganList == "All")
+            if (CodeTextBox.Text == myCode)
             {
-                selectedOrgan = "All";
-            }
-            else
-            {
+                string phone = String.Format("{0}", Request.Form["phone"]);
+                long PhoneNumber = long.Parse(phone);
+                string n = String.Format("{0}", Request.Form["DateOfBirthTextBox"]);
 
-                foreach (ListItem lst in CheckBoxList1.Items)
+                string selectOrganList = Request.Form["Donatestate"];
+
+                string selectedOrgan = "";
+
+                if (selectOrganList == "All")
                 {
-                    if (lst.Selected == true)
+                    selectedOrgan = "All";
+                }
+                else
+                {
+
+                    foreach (ListItem lst in CheckBoxList1.Items)
                     {
-                        selectedOrgan += lst.Text + " , ";
+                        if (lst.Selected == true)
+                        {
+                            selectedOrgan += lst.Text + " , ";
+                        }
                     }
                 }
-            }
-          
-          
 
-            try
+
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("Insert_Organs", con);
+
+                    con.Open();
+
+                    cmd.Parameters.Add("@firstName", SqlDbType.NVarChar).Value = FirstNameTextBox.Text;
+                    cmd.Parameters.Add("@lastName", SqlDbType.VarChar).Value = LastNameTextBox.Text;
+
+                    cmd.Parameters.Add("@phoneNumber", SqlDbType.VarChar).Value = PhoneNumber;
+                    cmd.Parameters.Add("@email", SqlDbType.NVarChar).Value = EmailTextBox.Text;
+
+                    cmd.Parameters.Add("@gender", SqlDbType.VarChar).Value = GenderDropDownList.SelectedItem.Text;
+                    cmd.Parameters.Add("@address", SqlDbType.NVarChar).Value = AddressTextBox.Text;
+
+
+                    cmd.Parameters.Add("@dateOfBirth", SqlDbType.NVarChar).Value = Convert.ToDateTime(n);
+                    cmd.Parameters.Add("@bloodGroup", SqlDbType.VarChar).Value = BloodTypeDropDownList.SelectedItem.Text;
+
+                    cmd.Parameters.Add("@city", SqlDbType.VarChar).Value = CityTextBox.Text;
+                    cmd.Parameters.Add("@organs", SqlDbType.NVarChar).Value = selectedOrgan;
+
+                    cmd.Parameters.Add("@signature", SqlDbType.VarChar).Value = SignatureTextBox.Text;
+                    cmd.Parameters.Add("@bloodDoner", SqlDbType.NVarChar).Value = BloodAnswerDropDownList.SelectedItem.Text;
+
+                    cmd.Parameters.Add("@usedOrgansFor", SqlDbType.VarChar).Value = UseOrgansForDropDownList0.SelectedItem.Text;
+                    cmd.Parameters.Add("@date", SqlDbType.Date).Value = DateTime.Now;
+
+
+
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                if (GenderDropDownList.SelectedIndex == 0)
+                {
+
+                }
+            }
+
+            else
             {
-                SqlCommand cmd = new SqlCommand("Insert_Organs", con);
-
-                con.Open();
-
-                cmd.Parameters.Add("@firstName", SqlDbType.NVarChar).Value = FirstNameTextBox.Text;
-                cmd.Parameters.Add("@lastName", SqlDbType.VarChar).Value = LastNameTextBox.Text;
-
-                cmd.Parameters.Add("@phoneNumber", SqlDbType.VarChar).Value = PhoneNumber;
-                cmd.Parameters.Add("@email", SqlDbType.NVarChar).Value = EmailTextBox.Text;
-
-                cmd.Parameters.Add("@gender", SqlDbType.VarChar).Value = GenderDropDownList.SelectedItem.Text;
-                cmd.Parameters.Add("@address", SqlDbType.NVarChar).Value = AddressTextBox.Text;
-
-
-                cmd.Parameters.Add("@dateOfBirth", SqlDbType.NVarChar).Value = Convert.ToDateTime(n);
-                cmd.Parameters.Add("@bloodGroup", SqlDbType.VarChar).Value = BloodTypeDropDownList.SelectedItem.Text;
-
-                cmd.Parameters.Add("@city", SqlDbType.VarChar).Value = CityTextBox.Text;
-                cmd.Parameters.Add("@organs", SqlDbType.NVarChar).Value = selectedOrgan;
-
-                cmd.Parameters.Add("@signature", SqlDbType.VarChar).Value = SignatureTextBox.Text;
-                cmd.Parameters.Add("@bloodDoner", SqlDbType.NVarChar).Value = BloodAnswerDropDownList.SelectedItem.Text;
-
-                cmd.Parameters.Add("@usedOrgansFor", SqlDbType.VarChar).Value = UseOrgansForDropDownList0.SelectedItem.Text;
-                cmd.Parameters.Add("@date", SqlDbType.Date).Value = DateTime.Now;
-
-                
-
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.ExecuteNonQuery();
-                con.Close();
+                System.Web.UI.ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AlertBox", "alert('Invalid code the code is: ');" + myCode, true);
             }
-            catch (Exception)
-            {
 
-                throw;
-            }
-            if (GenderDropDownList.SelectedIndex == 0)
-            {
-             
-            }
+
+        }
+
+        protected void SendCodeButton1_Click(object sender, EventArgs e)
+        {
+            string phone = String.Format("{0}", Request.Form["phone"]);
+            CodeTextBox.Text = phone;
+            g.generator();
+            myCode = g.code;
+
+            long PhoneNumber = long.Parse(phone);
+
+            const string YourAccessKey = "NrCjD40h6gaCws2A0t0VEFVXW"; // your access key here
+            Client client = Client.CreateDefault(YourAccessKey);
+            long Msisdn = PhoneNumber; // your phone number here
+            MessageBird.Objects.Message message =
+            client.SendMessage("HealthBook", myCode, new[] { Msisdn });
+
+            SendCodeButton1.Attributes.Add("onclick", "return false;");
 
         }
     }
